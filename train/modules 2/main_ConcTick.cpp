ce3d208a29d4cd75cdd23c3f151125e2c1942d7e
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-04-15 15:26:26 -0300

    Create main_ConcTick.cpp
    add

---
+++ train/modules 2/main_ConcTick.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "bookTick.h"
+
+//interação com usuário
+int main(){
+ 
+    std::cout << "Quantos ingressos você gostaria de reservar? ";
+    std::cin >> numTick;
+
+    return 0;
+
+}
