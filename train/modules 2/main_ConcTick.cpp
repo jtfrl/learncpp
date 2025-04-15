e5e11c583b26440f9e4cdbf3bc7501722b82d72a
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-04-15 15:44:41 -0300

    Update main_ConcTick.cpp
    add falha do codespace

--- train/modules 2/main_ConcTick.cpp
+++ train/modules 2/main_ConcTick.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "bookTick.h"
+#include "bookTick.hpp"
+
+/*
+#ifndef BTICK_H
+#define BTICK_H
+
+int bookTick(int numTick);
+
+#endif 
+*/
 
 //interação com usuário
 int main(){
- 
-    std::cout << "Quantos ingressos você gostaria de reservar? ";
+    int numTick;
+    std::cout << "Quantos ingressos você gostaria de reservar? "<< std::endl;
     std::cin >> numTick;
 
     return 0;
