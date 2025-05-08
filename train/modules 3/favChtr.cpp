194570cd626018240237c7e043f138c2a6de700d
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-08 12:26:48 -0300

    Update favChtr.cpp
    add m_chtr

--- train/modules 3/favChtr.cpp
+++ train/modules 3/favChtr.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "favChtr.hpp"
 
+int favorite::m_chtr=0; //inicializa o contador de personagens
+
 favorite::favorite(std::string character, std::string series, int age, int seriesYear):
      m_character(character), m_series(series), m_age(age), m_seriesYear(seriesYear)
-{}//construtor que permite adicionar os itens de personagem preferidos.
+{
+    m_chtr++;
+}//construtor que permite adicionar os itens de personagem preferidos.
 
 
 void favorite::setCharacter(std::string novoChtr){
     m_character=novoChtr;
+    m_chtr++;
 }
 
 std::string favorite::getCharacter(){
@@ -30,10 +35,15 @@ void favorite::setAge(int age){
 int favorite::getcAge(){
     return m_age;
 }
+
 void favorite::setseriesYear(int seriesYear){
     if (seriesYear>0)  m_seriesYear=seriesYear;
     std::cout<<"Digite um número válido"<<std::endl;
 }
 int favorite::getsYear(){
     return m_seriesYear;
+}
+
+int favorite::countFav(){
+   return m_chtr;
 }
