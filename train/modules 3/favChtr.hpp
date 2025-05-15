7deeb046869e9883d45b18c388103cdbfca8f14b
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-15 14:28:26 -0300

    Update favChtr.hpp
    add displaySeries

--- train/modules 3/favChtr.hpp
+++ train/modules 3/favChtr.hpp
@@ -1,29 +1,69 @@
-#include <string> //necessário para usar string
-#include <vector>
-class favorite{
-    std::string m_character;
-    std::string m_series;
-    int m_age=0;
-    int m_seriesYear=0;
-    static int m_chtr;
-    static int m_sCount;
-    static std::vector<std::string> m_allSeries; //vetor que armazena os nomes
+#include <iostream>
+#include "favChtr.hpp"
 
-    public:
-    //favorite()=default;
-    favorite(); //construtor padrão
-    favorite(std::string character, std::string series, int age, int seriesYear);
 
-    void setCharacter(std::string novoChtr);
-    std::string getCharacter();
-    void setSeries(std::string seriesName);
-    std::string getSeries();
-    void setAge(int age);
-    int getcAge();
-    void setseriesYear(int seriesYear);
-    int getsYear();
-    static int countFav(); //deve ser estático
-    static int countSeries();
-    std::string displaySeries(); //dessa forma, passa a pertencer entre os métodos da classe
+int favorite::m_chtr=0; //inicializa o contador de personagens
+int favorite::m_sCount=0;
+std::vector<std::string> favorite::m_allSeries; //inicializa o estático de nomes
 
-};
+//inicializa para m_allSeries
+
+
+favorite::favorite(std::string character, std::string series, int age, int seriesYear):
+     m_character(character), m_series(series), m_age(age), m_seriesYear(seriesYear)
+{
+    m_chtr++;
+    m_sCount++;
+    m_allSeries.push_back(series); //as series vão para o vetor m_allSeries
+}//construtor que permite adicionar os itens de personagem preferidos.
+
+
+void favorite::setCharacter(std::string novoChtr){
+    m_character=novoChtr;
+    m_chtr++;
+}
+
+std::string favorite::getCharacter(){
+    return m_character;
+}
+
+void favorite::setSeries(std::string seriesName){
+    m_series=seriesName;
+    m_sCount++;
+}
+
+std::string favorite::getSeries(){
+    return m_series;
+}
+
+void favorite::displaySeries(){
+    std::cout<<"Series Names"<<std::endl;
+
+    for (const auto& series: m_allSeries) std::cout<<series<<std::endl;
+} //função estática: pode usar m_series
+//utiliza referência e tipo auto
+
+void favorite::setAge(int age){
+    if (age>0) m_age=age;
+    std::cout<<"Digite um número válido"<<std::endl;
+}
+
+int favorite::getcAge(){
+    return m_age;
+}
+
+void favorite::setseriesYear(int seriesYear){
+    if (seriesYear>0)  m_seriesYear=seriesYear;
+    std::cout<<"Digite um número válido"<<std::endl;
+}
+int favorite::getsYear(){
+    return m_seriesYear;
+}
+
+int favorite::countFav(){
+   return m_chtr;
+}
+
+int favorite::countSeries(){
+    return m_sCount;
+}
