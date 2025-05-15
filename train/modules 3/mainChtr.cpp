c7c7a907488e8fbbc9afe6d4f2e2c9e6019c8ac2
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-15 14:27:40 -0300

    Update mainChtr.cpp
    add displaySeries()

--- train/modules 3/mainChtr.cpp
+++ train/modules 3/mainChtr.cpp
@@ -4,7 +4,7 @@
 int main(){
     favorite td("Rust Cohle", "True Detective", 29, 2014); //cria uma variavel para a classe favorite
     favorite fb("The Priest", "Fleabg", 35, 2016); //cria uma variavel para a classe favorite
-    
+
     std::cout<<"Personagem favorito: "<<td.getCharacter()<<std::endl;
     std::cout<<"Série favorita: "<<td.getSeries()<<std::endl;
     std::cout<<"Idade do personagem: "<<td.getcAge()<<std::endl;
@@ -19,11 +19,12 @@ int main(){
   std::cout<<"Ano (série): "<<fb.getsYear()<<std::endl;
 
   std::cout<<"Personagens adicionados: "<<favorite::countFav()<<std::endl;
+  std::cout<<std::endl;
   std::cout<<"******************************"<<"\n";
   std::cout<<"Séries adicionadas: "<<std::endl;
   std::cout<<"Total: "<<favorite::countSeries()<<std::endl;
   favorite::displaySeries();
-  //adicionar contagem filtrada por série 
+  //adicionar contagem filtrada por série
 
 
     return 0;
