dbb6a8dad623fb59de9afc8cf0b0757d3e547e26
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-15 14:29:23 -0300

    Update favChtr.cpp
    add displaySeries

--- train/modules 3/favChtr.cpp
+++ train/modules 3/favChtr.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "favChtr.hpp"
 
+
 int favorite::m_chtr=0; //inicializa o contador de personagens
-int favorite::m_sCount=0; 
+int favorite::m_sCount=0;
 std::vector<std::string> favorite::m_allSeries; //inicializa o estático de nomes
+
+//inicializa para m_allSeries
 
 
 favorite::favorite(std::string character, std::string series, int age, int seriesYear):
      m_character(character), m_series(series), m_age(age), m_seriesYear(seriesYear)
 {
     m_chtr++;
+    m_sCount++;
+    m_allSeries.push_back(series); //as series vão para o vetor m_allSeries
 }//construtor que permite adicionar os itens de personagem preferidos.
 
 
@@ -31,10 +36,12 @@ std::string favorite::getSeries(){
     return m_series;
 }
 
-std::string favorite::displaySeries(){
+void favorite::displaySeries(){
     std::cout<<"Series Names"<<std::endl;
-    return m_series;
-}
+
+    for (const auto& series: m_allSeries) std::cout<<series<<std::endl;
+} //função estática: pode usar m_series
+//utiliza referência e tipo auto
 
 void favorite::setAge(int age){
     if (age>0) m_age=age;
@@ -60,4 +67,3 @@ int favorite::countFav(){
 int favorite::countSeries(){
     return m_sCount;
 }
-
