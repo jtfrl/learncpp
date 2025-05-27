dffd5cc889ccb04e75084514c953ac8e59bb34da
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-27 19:26:27 +0000

    add deliveryMan

--- train/delivComp/DeliveryManager.h
+++ train/delivComp/DeliveryManager.h
@@ -1,11 +1,36 @@
-template <typename T>
+#include <iostream>
+#include <vector>
+
+template <typename T> //a ideia é que T seja trocado ou por waterGall ou pelo outro
 class DeliveryManager {
 private:
-    std::vector<T> inventory;
-    T price; //pode ser um double aqui
+    std::vector<T> inventory; //aqui vem os dois tipos criados abaixo
+
+    struct waterGal{
+        double volume, weight, price;
+        std::string brand;
+    };
+    struct gasCyl{
+        double volCap, price;
+        std::string saftyCert, gasType;
+    };
+
 public:
+    DeliveryManager(T item);
     void addItem(const T& item);
+    //overloading aqui
+
     void removeItem(int index);
     void displayInventory() const;
+
+    double totalWt(T u_weight);
+    double totalVl(T u_volume); //esse e o primeiro para waterGal
+    double totalVC(T u_volCap);
     // Add other methods as needed
 };
+
+template<class T>
+DeliveryManager<T>::DeliveryManager(T item){
+    u_item=item;
+}
+
